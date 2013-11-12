1336f572565a0e8930548a2f36a7d1e07de8c891
Andrey Malakhov <a.malakhov@mobidev.biz>
2013-11-12 13:01:42 +0200

    Generate extends Generate body

--- annotation_processor/src/blak/annotations/simple/RepeatProcessor.java
+++ annotation_processor/src/blak/annotations/simple/RepeatProcessor.java
@@ -1,12 +1,29 @@
 package blak.annotations.simple;
 
+import blak.annotations.OriginatingElements;
+import blak.annotations.ResourceCodeWriter;
+import blak.annotations.SourceCodeWriter;
+import com.sun.codemodel.JBlock;
+import com.sun.codemodel.JClass;
+import com.sun.codemodel.JClassAlreadyExistsException;
+import com.sun.codemodel.JCodeModel;
+import com.sun.codemodel.JDefinedClass;
+import com.sun.codemodel.JExpr;
+import com.sun.codemodel.JInvocation;
+import com.sun.codemodel.JMethod;
+import com.sun.codemodel.JMod;
+import com.sun.codemodel.JVar;
+
 import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Filer;
+import javax.annotation.processing.Messager;
 import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
@@ -19,38 +36,42 @@ public class RepeatProcessor extends AbstractProcessor {
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        printWarning("Root elements");
-        for (Element elem : roundEnv.getRootElements()) {
-            printWarning(elem);
-            TypeElement typeElement = (TypeElement) elem;
-            for (Element element : typeElement.getEnclosedElements()) {
-                printWarning("   ", element);
+        //roundEnv.getRootElements().contains()
+        for (Element element : roundEnv.getElementsAnnotatedWith(RActivity.class)) {
+            try {
+                TypeElement typeElement = (TypeElement) element;
+                String qualifiedName = typeElement.getQualifiedName().toString();
+                JCodeModel codeModel = new JCodeModel();
+
+                JDefinedClass clazz = codeModel._class(qualifiedName + GENERATION_SUFFIX);
+
+                // extends
+                JClass annotatedActivity = codeModel.directClass(qualifiedName);
+                clazz._extends(annotatedActivity);
+                //generateExtends();
+
+                generateInit(codeModel, clazz);
+
+                generateClassFile(codeModel);
+            } catch (JClassAlreadyExistsException e) {
+                e.printStackTrace();
+                printWarning(e.getMessage());
             }
         }
 
-        printWarning("Annotated with", Repeat.class);
-        for (Element el : roundEnv.getElementsAnnotatedWith(Repeat.class)) {
-            printWarning(el);
-        }
-
-        //try {
-        //    JCodeModel codeModel = new JCodeModel();
-        //    Filer filer = processingEnv.getFiler();
-        //    Messager messager = processingEnv.getMessager();
-        //
-        //    SourceCodeWriter sourceCodeWriter = new SourceCodeWriter(filer, messager, new OriginatingElements());
-        //    JDefinedClass definedClass = codeModel._class("com.example.MyNewClass");
-        //    codeModel.build(sourceCodeWriter, new ResourceCodeWriter(filer));
-        //    //JFieldVar field = definedClass.field(JMod.PRIVATE, int.class, "intVar");
-        //} catch (JClassAlreadyExistsException e) {
-        //    e.printStackTrace();
-        //    printWarning(e.getMessage());
-        //} catch (IOException e) {
-        //    e.printStackTrace();
-        //    printWarning(e.getMessage());
-        //}
-
         return true;
+    }
+
+    private void generateInit(JCodeModel codeModel, JDefinedClass clazz) {
+        JMethod init = clazz.method(JMod.PUBLIC, codeModel.INT, "init");
+
+        init.annotate(Deprecated.class);
+
+        JVar paramAlpha = init.param(codeModel.INT, "alpha");
+
+        JBlock body = init.body();
+
+        body._return(paramAlpha);
     }
 
     @Override
@@ -75,6 +96,34 @@ public class RepeatProcessor extends AbstractProcessor {
 
     private void printWarning(Object... messages) {
         printWarning(join(messages));
+    }
+
+    private void printElements(RoundEnvironment roundEnv) {
+        printWarning("Root elements");
+        for (Element elem : roundEnv.getRootElements()) {
+            printWarning(elem);
+            TypeElement typeElement = (TypeElement) elem;
+            for (Element element : typeElement.getEnclosedElements()) {
+                printWarning("   ", element);
+            }
+        }
+
+        printWarning("Annotated with", Repeat.class);
+        for (Element el : roundEnv.getElementsAnnotatedWith(Repeat.class)) {
+            printWarning(el);
+        }
+    }
+
+    private void generateClassFile(JCodeModel codeModel) {
+        try {
+            Filer filer = processingEnv.getFiler();
+            Messager messager = processingEnv.getMessager();
+            SourceCodeWriter sourceCodeWriter = new SourceCodeWriter(filer, messager, new OriginatingElements());
+            codeModel.build(sourceCodeWriter, new ResourceCodeWriter(filer));
+        } catch (IOException e) {
+            e.printStackTrace();
+            printWarning(e.getMessage());
+        }
     }
 
     public static String join(Object... params) {
---
+++ custom_annotations_sample/src/com/example/annotations/CodeGeneration.java
@@ -0,0 +1,57 @@
+package com.example.annotations;
+
+import com.sun.codemodel.JBlock;
+import com.sun.codemodel.JClass;
+import com.sun.codemodel.JCodeModel;
+import com.sun.codemodel.JDefinedClass;
+import com.sun.codemodel.JExpr;
+import com.sun.codemodel.JExpression;
+import com.sun.codemodel.JMethod;
+import com.sun.codemodel.JMod;
+import com.sun.codemodel.JVar;
+
+import java.io.File;
+
+public class CodeGeneration {
+    private static final String GENERATION_SUFFIX = "_";
+
+    public static void main(String[] args) throws Exception {
+
+        String qualifiedName = "com.example.annotations.Code";
+
+        JCodeModel codeModel = new JCodeModel();
+        JDefinedClass clazz = codeModel._class(qualifiedName + GENERATION_SUFFIX);
+
+        generateExtends(codeModel, clazz, qualifiedName);
+
+        generateInit(codeModel, clazz);
+
+
+        File destDir = new File("custom_annotations_sample/gen");
+        destDir.mkdirs();
+        codeModel.build(destDir);
+    }
+
+    private static void generateExtends(JCodeModel codeModel, JDefinedClass clazz, String qualifiedName) {
+        //JClass annotatedActivity = codeModel.directClass(qualifiedName);
+        //clazz._extends(annotatedActivity);
+    }
+
+    private static void generateInit(JCodeModel codeModel, JDefinedClass clazz) {
+        JMethod init = clazz.method(JMod.PUBLIC, codeModel.INT, "init");
+
+        init.annotate(Deprecated.class);
+
+        JVar value = init.param(codeModel.CHAR, "value");
+
+        JClass stringType = codeModel.ref(String.class);
+
+        JBlock body = init.body();
+        JVar var = body.decl(stringType, "var");
+        var.init(JExpr._new(stringType));
+        body.assign(var, JExpr.direct("\"TestString\""));
+
+        JExpression result = JExpr.direct("var.charAt(100)");
+        body._return(result);
+    }
+}
