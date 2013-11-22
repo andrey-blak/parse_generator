6cc68e22e469f6393bb9442caeff9859a341984c
Andrey Malakhov <a.malakhov@mobidev.biz>
2013-11-22 12:55:19 +0200

    Refactoring

--- annotation_processor/src/blak/annotations/json/JsonProcessor.java
+++ annotation_processor/src/blak/annotations/json/JsonProcessor.java
@@ -22,22 +22,16 @@ import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.AnnotationMirror;
-import javax.lang.model.element.AnnotationValue;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
-import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.TypeElement;
-import javax.lang.model.type.DeclaredType;
 import javax.lang.model.type.TypeKind;
 import javax.lang.model.type.TypeMirror;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Set;
 
 // TODO
-// refactor - add holder
 // class parser
 // methods
 // enum
@@ -65,76 +59,78 @@ public class JsonProcessor extends BaseProcessor {
     private static final String OPT_JSON_OBJECT = "optJSONObject";
     private static final String JSON1 = "Json";
 
+    private JCodeModel mCodeModel;
+
     @Override
     public boolean processAnnotations(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        JCodeModel codeModel = new JCodeModel();
+        mCodeModel = new JCodeModel();
         Set<? extends Element> rootElements = roundEnv.getElementsAnnotatedWith(XmlRootElement.class);
         for (Element rootElement : rootElements) {
             try {
-                process(codeModel, roundEnv, (TypeElement) rootElement);
+                process(roundEnv, (TypeElement) rootElement);
             } catch (JClassAlreadyExistsException e) {
                 e.printStackTrace();
             }
         }
-        buildModel(codeModel);
+        buildModel(mCodeModel);
 
         return false;
     }
 
-    private void process(JCodeModel codeModel, RoundEnvironment roundEnv, TypeElement rootElement) throws JClassAlreadyExistsException {
+    private void process(RoundEnvironment roundEnv, TypeElement rootElement) throws JClassAlreadyExistsException {
         String qualifiedName = rootElement.getQualifiedName().toString();
-        JDefinedClass clazz = codeModel._class(qualifiedName + JSON_PARSER);
+        JDefinedClass clazz = mCodeModel._class(qualifiedName + JSON_PARSER);
 
         String simpleName = CodeUtils.getName(rootElement);
-        JType rootElementType = codeModel.directClass(simpleName);
+        JType rootElementType = mCodeModel.directClass(simpleName);
 
         JMethod parse = clazz.method(JMod.PUBLIC, rootElementType, PARSE);
-        JVar jsonString = parse.param(codeModel.ref(STRING), JSON_STRING);
+        JVar jsonString = parse.param(mCodeModel.ref(STRING), JSON_STRING);
         JBlock body = parse.body();
 
-        JClass jsonObjectType = codeModel.ref(JSONObject.class);
+        JClass jsonObjectType = mCodeModel.ref(JSONObject.class);
 
         JVar json = body.decl(jsonObjectType, JSON, JExpr._new(jsonObjectType).arg(jsonString));
         JVar dto = body.decl(rootElementType, DTO, JExpr._new(rootElementType));
 
-        processElements(codeModel, body, dto, json, rootElement);
+        processElements(body, dto, json, rootElement);
 
         body._return(dto);
     }
 
-    private void processElements(JCodeModel codeModel, JBlock body, JExpression dto, JExpression json, Element rootElement) {
+    private void processElements(JBlock body, JExpression dto, JExpression json, Element rootElement) {
         for (Element element : rootElement.getEnclosedElements()) {
-            AnnotationMirror mirror = findAnnotationValue(element, XmlElement.class);
+            AnnotationMirror mirror = CodeUtils.findAnnotationValue(element, XmlElement.class);
             if (mirror == null) {
                 continue;
             }
 
             if (element.getKind() == ElementKind.FIELD) {
                 String fieldName = CodeUtils.getName(element);
-                String key = extractValue(mirror, NAME, String.class, fieldName);
+                String key = CodeUtils.extractValue(mirror, NAME, String.class, fieldName);
 
-                optFieldValue(codeModel, body, dto, json, element, key);
+                optFieldValue(body, dto, json, element, key);
             }
         }
     }
 
-    private void optFieldValue(JCodeModel codeModel, JBlock body, JExpression dto, JExpression json, Element field, String key) {
+    private void optFieldValue(JBlock body, JExpression dto, JExpression json, Element field, String key) {
         String typeString = field.asType().toString();
 
         if (char.class.getName().equals(typeString) || Character.class.getName().equals(typeString)) {
-            optCharValue(codeModel, body, dto, json, field, key);
+            optCharValue(body, dto, json, field, key);
         } else {
             String jsonGetType = JsonUtils.getGetType(typeString);
             if (jsonGetType != null) {
-                optPrimitiveValue(codeModel, body, dto, json, field, key, typeString, jsonGetType);
+                optPrimitiveValue(body, dto, json, field, key, typeString, jsonGetType);
             } else {
                 JBlock block = body.block();
-                optClassValue(codeModel, block, dto, json, field, key, typeString);
+                optClassValue(block, dto, json, field, key, typeString);
             }
         }
     }
 
-    private void optPrimitiveValue(JCodeModel codeModel, JBlock body, JExpression dto, JExpression json, Element field, String key, String typeString, String jsonGetType) {
+    private void optPrimitiveValue(JBlock body, JExpression dto, JExpression json, Element field, String key, String typeString, String jsonGetType) {
         String getMethod = OPT + jsonGetType;
 
         JExpressionImpl getValue = json.invoke(getMethod).arg(key);
@@ -145,7 +141,7 @@ public class JsonProcessor extends BaseProcessor {
                 // todo get wrapped type
                 typeString = typeString.toLowerCase();
             }
-            castType = codeModel.ref(typeString);
+            castType = mCodeModel.ref(typeString);
             getValue = JExpr.cast(castType, getValue);
         }
 
@@ -154,11 +150,11 @@ public class JsonProcessor extends BaseProcessor {
         body.assign(fieldRef, getValue);
     }
 
-    private void optCharValue(JCodeModel codeModel, JBlock body, JExpression dto, JExpression json, Element field, String key) {
+    private void optCharValue(JBlock body, JExpression dto, JExpression json, Element field, String key) {
         JBlock block = body.block();
         String fieldName = CodeUtils.getName(field);
         JExpression getString = json.invoke(OPT_STRING).arg(key);
-        JVar charString = block.decl(codeModel.ref(STRING), fieldName + STRING, getString);
+        JVar charString = block.decl(mCodeModel.ref(STRING), fieldName + STRING, getString);
 
         JExpression notEmpty = charString.invoke(IS_EMPTY).not();
         JExpression firstChar = charString.invoke(CHAR_AT).arg(JExpr.lit(0));
@@ -168,42 +164,20 @@ public class JsonProcessor extends BaseProcessor {
         block.assign(fieldRef, getChar);
     }
 
-    private void optClassValue(JCodeModel codeModel, JBlock block, JExpression dto, JExpression json, Element field, String key, String typeString) {
+    private void optClassValue(JBlock block, JExpression dto, JExpression json, Element field, String key, String typeString) {
         String fieldName = CodeUtils.getName(field);
         JFieldRef fieldRef = dto.ref(fieldName);
         TypeMirror typeMirror = field.asType();
 
-        JType fieldType = codeModel.directClass(typeMirror.toString());
+        JType fieldType = mCodeModel.directClass(typeMirror.toString());
         block.assign(fieldRef, JExpr._new(fieldType));
 
         JExpressionImpl getString = json.invoke(OPT_JSON_OBJECT).arg(key);
-        JClass jsonObjectType = codeModel.ref(JSONObject.class);
+        JClass jsonObjectType = mCodeModel.ref(JSONObject.class);
         JVar fieldJson = block.decl(jsonObjectType, fieldName + JSON1, getString);
 
         Element fieldTypeElement = processingEnv.getTypeUtils().asElement(typeMirror);
-        processElements(codeModel, block, fieldRef, fieldJson, fieldTypeElement);
-    }
-
-    private static <T> AnnotationMirror findAnnotationValue(Element element, Class<T> annotationClass) {
-        for (AnnotationMirror annotationMirror : element.getAnnotationMirrors()) {
-            DeclaredType annotationType = annotationMirror.getAnnotationType();
-            TypeElement annotationElement = (TypeElement) annotationType.asElement();
-            if (annotationElement.getQualifiedName().contentEquals(annotationClass.getCanonicalName())) {
-                return annotationMirror;
-            }
-        }
-        return null;
-    }
-
-    private static <T> T extractValue(AnnotationMirror annotationMirror, String valueName, Class<T> expectedType, T defValue) {
-        Map<ExecutableElement, AnnotationValue> elementValues = new HashMap<ExecutableElement, AnnotationValue>(annotationMirror.getElementValues());
-        for (Map.Entry<ExecutableElement, AnnotationValue> entry : elementValues.entrySet()) {
-            if (entry.getKey().getSimpleName().contentEquals(valueName)) {
-                Object value = entry.getValue().getValue();
-                return expectedType.cast(value);
-            }
-        }
-        return defValue;
+        processElements(block, fieldRef, fieldJson, fieldTypeElement);
     }
 
     @Override
--- annotation_processor/src/blak/annotations/utils/CodeUtils.java
+++ annotation_processor/src/blak/annotations/utils/CodeUtils.java
@@ -1,9 +1,38 @@
 package blak.annotations.utils;
 
+import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.AnnotationValue;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.DeclaredType;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CodeUtils {
     public static String getName(Element element) {
         return element.getSimpleName().toString();
+    }
+
+    public static <T> AnnotationMirror findAnnotationValue(Element element, Class<T> annotationClass) {
+        for (AnnotationMirror annotationMirror : element.getAnnotationMirrors()) {
+            DeclaredType annotationType = annotationMirror.getAnnotationType();
+            TypeElement annotationElement = (TypeElement) annotationType.asElement();
+            if (annotationElement.getQualifiedName().contentEquals(annotationClass.getCanonicalName())) {
+                return annotationMirror;
+            }
+        }
+        return null;
+    }
+
+    public static <T> T extractValue(AnnotationMirror annotationMirror, CharSequence valueName, Class<T> expectedType, T defValue) {
+        Map<ExecutableElement, AnnotationValue> elementValues = new HashMap<ExecutableElement, AnnotationValue>(annotationMirror.getElementValues());
+        for (Map.Entry<ExecutableElement, AnnotationValue> entry : elementValues.entrySet()) {
+            if (entry.getKey().getSimpleName().contentEquals(valueName)) {
+                Object value = entry.getValue().getValue();
+                return expectedType.cast(value);
+            }
+        }
+        return defValue;
     }
 }
